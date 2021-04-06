d5770b88eff4dff3eacc8af16b2e476810b88b83
Quentin Cruz <napu0515@gmail.com>
2021-04-06 11:51:11 -0700

    aboutME

---
+++ aboutME.js
@@ -0,0 +1,19 @@
+'use strict';
+ let userName = prompt('Hafa Adai, What is your name?')
+ console.log('Great to meet you '+ userName)
+alert ('Welcome to Me'+ ' ' + userName + '!')
+
+let island = prompt('What island am I from?,Hint: It starts with a G')
+
+if (island === 'Guam'|| island === 'guam'){
+  alert('Aye! Great Job '+ userName)
+} else {
+  alert('Its alright! Try Again!')
+}
+
+
+let profession = prompt('What did I do before Code Fellows?, )
+
+let degree = prompt('What did I get my degree in?')
+
+let industry = prompt('What type of developer would I like to become?')
